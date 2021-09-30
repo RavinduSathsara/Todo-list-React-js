dd1acc236dabb8c374cd3a23086179c8ae3657aa
RavinduSathsara <rsathsara330@gmail.com>
2021-09-30 12:24:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    type: "",
+    msg: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+      showAlert(true, "danger", "please enter todo");
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      showAlert(true, "success", "item add to the list");
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
+  return (
+    <section className="section-center">
+      <form className="todo-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>Todo List</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="todo"
+            placeholder="e.g go to school"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "add"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="todo-container">
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
